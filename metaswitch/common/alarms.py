98871189c894ac9353df5aa4afe87c8faab0dbee
chris-elford-metaswitch <chris.elford@metaswitch.com>
2016-02-10 16:23:37 +0000

    Fix passive-aggression.

--- metaswitch/common/alarms.py
+++ metaswitch/common/alarms.py
@@ -48,8 +48,10 @@ sendrequest = None
 def issue_alarm(process, identifier):
     # Clearwater nodes all have clearwater-infrastructure installed.
     # It includes a command-line script that can be used to issue an alarm.
-    # Rather than refactor clearwater-infrastructure, we re-use the function
-    # used by the script by importing it here.
+    # We import the function used by the script and re-use it.
+    #
+    # See https://github.com/Metaswitch/clearwater-infrastructure/blob/master/clearwater-infrastructure/usr/share/clearwater/bin/alarms.py
+    # for the target module.
     global sendrequest
     if sendrequest is None:
         try:
