8efcbf39c1a61578f7f5f07240eb0ba82ed089c8
Mat Meredith <matthew.meredith@metaswitch.com>
2017-05-26 13:13:13 +0000

    Add missing copyright headers

--- metaswitch/common/generate_stats_csv.py
+++ metaswitch/common/generate_stats_csv.py
@@ -1,3 +1,10 @@
+# Copyright (C) Metaswitch Networks 2017
+# If license terms are provided to you in a COPYING file in the root directory
+# of the source code repository by which you are accessing this code, then
+# the license outlined in that COPYING file applies to your use.
+# Otherwise no rights are granted except for those provided to you by
+# Metaswitch Networks in a separate written agreement.
+
 """Generate detailed statistics documentation.
 
 This script takes a number of MIB files and merges them with a CSV file
