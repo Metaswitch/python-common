ec8786f08b39aac4cce38ff58e8d640347f58a61
KristaHyer <krista.hyer@metaswitch.com>
2017-05-31 16:50:42 +0000

    pin py dep in python-common

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     setup_requires=["cffi"],
     ext_package="metaswitch.common",
     cffi_modules=["cffi_build.py:ffi"],
-    install_requires=["py-bcrypt", "pycrypto==2.6.1", "pyzmq==16.0.2", "cffi==1.5.2", "monotonic==0.6"],
+    install_requires=["py-bcrypt==0.4", "pycrypto==2.6.1", "pyzmq==16.0.2", "cffi==1.5.2", "monotonic==0.6"],
     tests_require=["pbr==1.6", "Mock", "phonenumbers==7.1.1"]
     )
